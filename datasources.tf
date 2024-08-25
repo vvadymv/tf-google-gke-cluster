05a868c0227bc4bd66084649ca4b15c609afbdcb
Vadym Vedmedenko <vadim.vedmedenko@gmail.com>
2024-08-25 23:51:58 +0300

    Fix data and output for module

---
+++ datasources.tf
@@ -0,0 +1,9 @@
+data "google_compute_instance_group" "node_instance_group" {
+#  self_link = module.gke_cluster.managed_instance_group_urls[0]
+  self_link = google_container_cluster.this.node_pool[0].managed_instance_group_urls[0]
+}
+
+data "google_compute_instance" "nodes" {
+  for_each  = toset(data.google_compute_instance_group.node_instance_group.instances[*])
+  self_link = each.key
+}
