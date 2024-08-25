7e299d035fca6ff9144707cf772df6f79df9bd2c
Vadym Vedmedenko <vadim.vedmedenko@gmail.com>
2024-08-25 20:01:30 +0300

    Initial google_container_cluster module

---
+++ main.tf
@@ -0,0 +1,20 @@
+resource "google_container_cluster" "this" {
+  name = "name_is_demo"
+  location = var.REGION
+
+  remove_default_node_pool = true
+  initial_node_count       = 1
+}
+
+resource "google_container_node_pool" "main" {
+  name       = "main"
+  project    = google_container_cluster.this.project
+  cluster    = google_container_cluster.this.name
+  location   = google_container_cluster.this.location
+  node_count = var.GKE_NUM_NODES
+
+  node_config {
+    machine_type = var.GKE_MACHINE_TYPE
+    disk_size_gb = 50
+  }
+}
---
+++ variables.tf
@@ -0,0 +1,18 @@
+variable "REGION" {
+  type        = string
+#  default     = "us-central1-a"
+  description = "GCP region name"
+}
+
+variable "GKE_NUM_NODES" {
+  type        = string
+  description = "Initial number of GKE cluster nodes"
+
+}
+
+variable "GKE_MACHINE_TYPE" {
+  type        = string
+#  default     = "e2-micro"
+  description = "GKE VM types"
+
+}
