9bb7e4b0f33b802b0836660190513b26c6cc5bf2
Vadym Vedmedenko <vadim.vedmedenko@gmail.com>
2024-08-26 00:58:03 +0300

    Fix deletion protection to allow GKE removal

--- main.tf
+++ main.tf
@@ -4,6 +4,7 @@ resource "google_container_cluster" "this" {
 
   remove_default_node_pool = true
   initial_node_count       = 1
+  deletion_protection = false
 }
 
 resource "google_container_node_pool" "main" {
